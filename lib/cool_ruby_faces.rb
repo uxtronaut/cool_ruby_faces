1f031f768cfd60be36349d86e2de4c9a1895c7b4
jmyers0022 <jmyers0022@gmail.com>
2015-05-20 23:27:56 -0600

    get specific cool faces (⌐■_■)

--- lib/cool_ruby_faces.rb
+++ lib/cool_ruby_faces.rb
@@ -3,6 +3,7 @@ require 'yaml'
 module CoolRubyFaces
 
   @faces = YAML.load(File.open(File.join(__dir__, 'faces.yml')))['faces']
+  @labels = YAML.load(File.open(File.join(__dir__, 'faces.yml')))['labels']
 
   def self.all
     return @faces.join("\n")
@@ -12,4 +13,7 @@ module CoolRubyFaces
     return @faces.shuffle.first
   end
 
+  def self.specific(face)
+    return @labels[face]
+  end
 end
